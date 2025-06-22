cd31f9fe53de9eff4a6baa9cfc457fd5e4a411ef
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-06-22 13:42:51 -0500

    feat(store): add userId state and fetchUser action.

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,9 +1,11 @@
 import { defineStore } from "pinia";
 import JwtService from "@/common/services/jwt.service.js";
+import ApiService from "@/common/services/api.service.js";
 
 export const useAuthStore = defineStore("auth", {
     state: () => ({
         token: JwtService.getToken() || null,
+        userId: JwtService.getUserId() || null,
         // You can store user data if needed
         user: null,
     }),
@@ -14,11 +16,23 @@ export const useAuthStore = defineStore("auth", {
         setToken(token) {
             this.token = token;
             JwtService.saveToken(token);
+            this.userId = JwtService.getUserId();
         },
         clearAuth() {
-            this.token = null;
             this.user = null;
+            this.token = null;
+            this.userId = null;
             JwtService.destroyToken();
+        },
+        async fetchUser() {
+            if (!this.userId)
+                return;
+            try {
+                const { data } = await ApiService.get(`/profiles/user/${this.userId}`);
+                this.user = data;
+            } catch {
+                this.clearAuth();
+            }
         },
     },
 });
